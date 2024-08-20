32f0a5c8306c2715b228ec96ade56109319ba2c2
28oso <28osoblanco2020@gmail.com>
2024-08-20 23:39:01 +0900

    clear: station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  var s = document.getElementById("check").checked
+  // console.log(s)
+  if (s) {
+    document.getElementById("text").style.backgroundColor = "red"
+  } else {
+    document.getElementById("text").style.backgroundColor = "transparent"
+  }
 }
